95fdce04e2d574009f99100ec7ee928fce03a14b
pradafrappa <logant9989@gmail.com>
2025-12-28 00:49:02 -0500

    Create main.lua

---
+++ loader/main.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 18450282790 then -- STEAL TIME SIMULATOR
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/pradafrappa/dropped-da-rice/refs/heads/main/steal_time_sim/index.lua"))()
+elseif game.PlaceId == 17306807164 then -- NO MERCY
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/pradafrappa/dropped-da-rice/refs/heads/main/no_mercy/ricehub.lua"))()
+else
+  game:GetService("StarterGui"):SetCore("SendNotification", {
+      Title = "Rice Hub",
+      Text = "Game not supported",
+      Duration = 5
+  })
+end
