42cc6acec9269da9f3ed8bc6fb6faeb44b4cf52d
pradafrappa <logant9989@gmail.com>
2025-12-28 23:41:55 -0500

    Update main.lua

--- loader/main.lua
+++ loader/main.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 18450282790 then -- STEAL TIME SIMULATOR
   loadstring(game:HttpGet("https://raw.githubusercontent.com/pradafrappa/dropped-da-rice/refs/heads/main/steal_time_sim/index.lua"))()
 elseif game.PlaceId == 17306807164 then -- NO MERCY
   loadstring(game:HttpGet("https://raw.githubusercontent.com/pradafrappa/dropped-da-rice/refs/heads/main/no_mercy/ricehub.lua"))()
+elseif game.PlaceId == 73703959264496 then -- FIGHT IN A SUPERMARKET
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/pradafrappa/dropped-da-rice/refs/heads/main/fiasm/supermarket.lua"))()
 else
   game:GetService("StarterGui"):SetCore("SendNotification", {
       Title = "Rice Hub",
